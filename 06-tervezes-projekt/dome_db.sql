4cbb63cf5834d996c6eeb33207f0d772683fd4c5
Nagy Zétény <nagyzeteny6@gmail.com>
2025-11-04 18:59:26 +0100

    add concession stands data

--- 06-tervezes-projekt/dome_db.sql
+++ 06-tervezes-projekt/dome_db.sql
@@ -13,7 +13,7 @@ CREATE TABLE `concession_stand` (
   `cash_registers` INT NOT NULL,
   `preorder` BOOLEAN NOT NULL DEFAULT FALSE,
   `pizza` BOOLEAN NOT NULL DEFAULT FALSE,
-  `nuggets` BOOLEAN NOT NULL DEFAULT FALSE,
+  `nuggets` BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE `supervisor` (
@@ -56,3 +56,20 @@ CREATE TABLE `stock` (
 );
 
 
+INSERT INTO `concession_stand` (`name`, `cash_registers`, `preorder`, `pizza`, `nuggets`) VALUES
+('fsz jobb', 4, FALSE, FALSE, FALSE),
+('fsz bal', 4, FALSE, FALSE, FALSE),
+('201', 6, TRUE, FALSE, TRUE),
+('204', 6, FALSE, TRUE, TRUE),
+('206', 6, FALSE, TRUE, FALSE),
+('207', 6, FALSE, FALSE, FALSE),
+('209', 6, TRUE, TRUE, TRUE),
+('212', 6, FALSE, FALSE, TRUE),
+('214', 4, FALSE, FALSE, FALSE),
+('215', 4, FALSE, FALSE, FALSE),
+('302/303', 4, FALSE, FALSE, FALSE),
+('307', 6, TRUE, FALSE, FALSE),
+('308', 6, TRUE, FALSE, FALSE),
+('312/313', 4, FALSE, FALSE, FALSE),
+('317', 6, FALSE, FALSE, FALSE),
+('318', 6, FALSE, FALSE, FALSE);
